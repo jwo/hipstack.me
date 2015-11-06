6281d9461c56dadc67d2f75e503a96b1c30823c6
Matthew Keas <matthiasak@gmail.com>
2015-11-06 12:21:57 -0600

    showing new example tests, and adding ava as build system instead of mocha

--- js/test/main.js
+++ js/test/main.js
@@ -1,3 +1,7 @@
+// TEST RUNNER (uses `ava`)
+// -----------------------------
+// https://github.com/sindresorhus/ava
+
 // -----------------------------
 // Unfortunately, while ES6 support is not yet fully released in
 // production to online platforms like Heroku, this file should stay
@@ -7,25 +11,14 @@
 // through the transpiler -- Babel -- in this package.json setup.
 // -----------------------------
 
-var mocha = require('mocha'),
-    chai = require('chai');
-
-var assert = chai.assert;
-var expect = chai.expect;
+import test from 'ava';
 
 //--- your setup code goes here (i.e. create test instances of your Constructors)
 // var Person = require('../Person.js').Person;
 //--- your setup code goes above here
 
-
-//--- example tests
-describe("Array", function(){ // a Constructor name
-    describe("#indexOf()", function(){ // a method of said Constructor
-        it("should return -1 when the value is not present", function(){
-            expect([1,2,3].indexOf(5)).to.equal(-1);
-            expect([1,2,3].indexOf(0)).to.equal(-1);
-        })
-    })
+test('Array#indexOf', t => {
+    t.plan(2) // expecting 2 tests to pass
+    t.is([1,2,3].indexOf(5), -1)
+    t.is([1,2,3].indexOf(0), -1)
 })
-
-//--- your tests go here
